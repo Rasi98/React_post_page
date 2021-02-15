b8a36293e01b32ccbe99b359a0db0fe04c5e756d
lakshan <49824628+Rasi98@users.noreply.github.com>
2021-02-15 11:32:37 +0530

    Material UI
    Installed material UI and tested

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+
+import memories from '../src/images/logo.jpg';
 
 const App=()=>{
     return(
-        <div>
-            <h1>Hello world!</h1>
-        </div>
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">POSTME</Typography>
+                <img src={memories} alt="Posts"></img>
+            </AppBar>
+        </Container>
     );
 }
 
