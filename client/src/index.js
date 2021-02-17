93b8c4418a27a1fb08381f20f478276a9f807e6f
lakshan <49824628+Rasi98@users.noreply.github.com>
2021-02-17 21:48:17 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {provider} from 'react-redux'; 
+import {createStore,applyMiddleware,compose} from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './app';
+import reducers from './reducers';
+
+const store=createStore(reducers,compose(applyMiddleware(thunk)));
 
 ReactDom.render(<App/>,document.getElementById('root'));
